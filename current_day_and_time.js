8537a6211dcf24ecacda5187603f3ffca4f05c6d
”ToavinaMaria” <ToavinaMaria>
2023-09-02 09:58:27 +0300

    feat : current_day comme date d'annif git add .

---
+++ current_day_and_time.js
@@ -0,0 +1,5 @@
+/**Anniversaire */
+var today = new Date("2024-01-30");
+var day=today.getDay();
+var daylist = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+console.log("Mon anniversaire sera le : " + daylist[day] + ".");
