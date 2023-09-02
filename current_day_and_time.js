689fa3688562c768668a98b296fe4351c2de08a9
”ToavinaMaria” <ToavinaMaria>
2023-09-02 10:54:44 +0300

    fix : current_dat_and_time normal.

--- current_day_and_time.js
+++ current_day_and_time.js
@@ -1,5 +1,35 @@
-/**Anniversaire */
-var today = new Date("2024-01-30");
+/**Date Anniversaire */
+var today = new Date();
 var day=today.getDay();
 var daylist = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-console.log("Mon anniversaire sera le : " + daylist[day] + ".");
+console.log("My birthdate will be in : " + daylist[day] + ".");
+/**Anniversaire */
+
+/*Heure actuelle====*/
+var hour = today.getHours();
+var minute = today.getMinutes();
+var second = today.getSeconds();
+
+var prepand = (hour >=12)? "PM" : "AM";
+hour = (hour>=12) ? hour -12:hour;
+
+if (hour===0 && prepand==="PM"){
+    if (minute===0 && second===0){
+        hour=12;
+        prepand='Noon';
+    } else{
+        hour=12;
+        prepand="PM";
+    }
+}
+    if(hour===0 && prepand ==='AM'){
+        if(minute===0 && second===0){
+            hour=12;
+            prepand="Midnight";
+        }else{
+            hour=12;
+            prepand='AM';
+        }
+    }
+console.log("Current time : " +hour+ prepand+": " +minute+": "+second);
+/*====Heure actuelle*/
